c3d7eaa0a821075f20cdcba8cfbaf8621b920ea2
olinkiss <olinkiss47@gmail.com>
2026-07-10 15:36:09 +0300

    Обмен чисел.

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значение переменной а типа int: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение переменной b типа int: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Ваши переменные: а = {a} и b = {b}.");
+
+int c = a;
+a = b;
+b = c;
+
+Console.Write($"Ваши переменные, поменяные местами: а = {a} и b = {b}");
