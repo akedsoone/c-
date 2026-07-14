72797b5b752a50ca41378cb4770455800b326bce
olinkiss <olinkiss47@gmail.com>
2026-07-14 23:00:31 +0300

    почти готовый калькутор, но пока не устойчивый

--- lesson3/calculator/Program.cs
+++ lesson3/calculator/Program.cs
@@ -1,6 +1,32 @@
-﻿int number = 0;
-int qestion = 0;
+﻿using System.ComponentModel.Design;
+
+int number = 0;
 bool qw = true;
+var al = "";
+
+int? addition = null;
+var additionA = 0;
+var additionB = 0;
+
+int? subtraction = 0;
+var subtractionA = 0;
+var subtractionB = 0;
+
+int? division = 0;
+var divisionA = 0;
+var divisionB = 0;
+
+int? multiplication = 0;
+int multiplicationA = 0;
+int multiplicationB = 0;
+
+int? percent = 0;
+int percentA = 0;
+int percentB = 0;
+
+int? square = 0;
+int squareA = 0;
+
 do
 {
     Console.WriteLine("КАЛЬКУЛЯТОР");
@@ -19,72 +45,107 @@ do
     {
         case 1:
             Console.WriteLine("Введите первое число для сложения:");
-            int additionA = Convert.ToInt32(Console.ReadLine());
+            additionA = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число для сложения:");
-            int AddititonB = Convert.ToInt32(Console.ReadLine());
-            int addition = additionA + AddititonB;
+            additionB = Convert.ToInt32(Console.ReadLine());
+            addition = additionA + additionB;
             Console.WriteLine($"Результат: {addition}");
             break;
 
         case 2:
             Console.WriteLine("Введите первое число для вычитания:");
-            int subtractionA = Convert.ToInt32(Console.ReadLine());
+            subtractionA = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число для вычитания:");
-            int subtractionB = Convert.ToInt32(Console.ReadLine());
-            int subtraction = subtractionA - subtractionB;
+            subtractionB = Convert.ToInt32(Console.ReadLine());
+            subtraction = subtractionA - subtractionB;
             Console.WriteLine($"Результат: {subtraction}");
             break;
 
         case 3:
             Console.WriteLine("Введите первое число для деления:");
-            int divisionA = Convert.ToInt32(Console.ReadLine());
+            divisionA = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число для деления:");
-            int divisionB = Convert.ToInt32(Console.ReadLine());
-            int division = divisionA / divisionB;
+            divisionB = Convert.ToInt32(Console.ReadLine());
+            division = divisionA / divisionB;
             Console.WriteLine($"Результат: {division}");
             break;
 
         case 4:
             Console.WriteLine("Введите первое число для умножения:");
-            int multiplicationA = Convert.ToInt32(Console.ReadLine());
+            multiplicationA = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число для умножения:");
-            int multiplicationB = Convert.ToInt32(Console.ReadLine());
-            int multiplication = multiplicationA * multiplicationB;
+            multiplicationB = Convert.ToInt32(Console.ReadLine());
+            multiplication = multiplicationA * multiplicationB;
             Console.WriteLine($"Результат: {multiplication}");
             break;
 
         case 5:
             Console.WriteLine("Введите число, от которого хотите найти процент:");
-            int percentA = Convert.ToInt32(Console.ReadLine());
+            percentA = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите число, для которого хотите найти процент:");
-            int percentB = Convert.ToInt32(Console.ReadLine());
-            int percent = (percentB * 100) / percentA;
+            percentB = Convert.ToInt32(Console.ReadLine());
+            percent = (percentB * 100) / percentA;
             Console.WriteLine($"Результат: {percent}");
             break;
 
         case 6:
             Console.WriteLine("Введите число, от которого хотите получить квадратный корень:");
-            int squareA = Convert.ToInt32(Console.ReadLine());
-            int square = squareA * squareA;
+            squareA = Convert.ToInt32(Console.ReadLine());
+            square = squareA * squareA;
             Console.WriteLine($"Результат: {square}");
             break;
 
         case 7:
-            Console.WriteLine("Результат:");
+            if (addition != 0)
+            {
+                Console.WriteLine($"Результат сложения {additionA} и {additionB}: {addition}");
+            }
+            if (subtraction != 0)
+            {
+                Console.WriteLine($"Результат вычитания {subtractionB} от {subtractionA}: {subtraction}");
+            }
+            if (division != 0)
+            {
+                Console.WriteLine($"Результат деления {divisionA} на {divisionB}: {division}"); 
+            }
+            if (multiplication != 0)
+            {
+                Console.WriteLine($"Результат умножения {multiplicationA} на {multiplicationB}: {multiplication}");
+            }
+            if (percent != 0)
+            {
+                Console.WriteLine($"Результат нахождения процента {percentB} от {percentA}: {percent}");
+            }
+            if (square != 0)
+            {
+                Console.WriteLine($"Результат нахождения квадрата числа {squareA}: {square}");
+            }
             break;
 
         default:
-            Console.WriteLine("Ввудите число от 1 до 7!");
+            Console.WriteLine("Введите число от 1 до 7!");
             break;
     }
-    Console.WriteLine("Желаете продолжить? (да/нет)");
-    var al = (Console.ReadLine());
-    if (al == "нет")
-    {
-        return;
-    }
-}
 
-while (qw);
+    do
+    {
+        Console.WriteLine("Желаете продолжить? (да/нет)");
+        al = (Console.ReadLine());
+        if (al == "да")
+        {
+            continue;
+        }
+        else if (al == "нет")
+        {
+            return;
+        }
+        else
+        {
+            Console.WriteLine("хуйню написал");
+           
+        }
+    } while (al != "да");
+    
+} while (qw);
 
 
