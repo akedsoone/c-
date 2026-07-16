7d3cab5e054f4dab3a216ca3068e953fc274e215
olinkiss <olinkiss47@gmail.com>
2026-07-16 14:36:49 +0300

    обернула код в do while, добавила задание про фибоначчи

--- lesson4/cycles/Program.cs
+++ lesson4/cycles/Program.cs
@@ -1,46 +1,82 @@
-﻿Console.WriteLine("Циклы");
-Console.WriteLine("1 Необходимо вывести на экран числа от N до 1.");
-Console.WriteLine("2 Необходимо, чтоб программа выводила на экран вот такую последовательность : 7 14 21 28 42 49….");
-Console.WriteLine("3 Вывод последовательности Фибоначчи");
-
-int choice = Convert.ToInt32(Console.ReadLine());
-
-switch (choice)
+﻿do
 {
-    case 1:
-        Console.WriteLine("Необходимо вывести на экран числа от N до 1.");
+    Console.WriteLine("Циклы");
+    Console.WriteLine("1 Необходимо вывести на экран числа от N до 1.");
+    Console.WriteLine("2 Необходимо, чтоб программа выводила на экран вот такую последовательность : 7 14 21 28 42 49….");
+    Console.WriteLine("3 Вывод последовательности Фибоначчи");
+    Console.WriteLine("4 Выход из программы");
 
-        Console.WriteLine("Введите число N");
-        int N = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Ваши числа:");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Необходимо вывести на экран числа от N до 1.");
+
+            Console.WriteLine("Введите число N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваши числа:");
 
 
-        for (int i = 1; i <= N; i++)
-        {
-            Console.Write(i);
-            Console.Write(" ");
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write(i);
+                Console.Write(" ");
 
-        }
+            }
+            Console.WriteLine("\n");
 
-        break;
+            break;
 
-    case 2:
-        Console.WriteLine("Необходимо, чтоб программа выводила на экран вот такую последовательность : 7 14 21 28 42 49….");
+        case 2:
+            Console.WriteLine("Необходимо, чтоб программа выводила на экран вот такую последовательность : 7 14 21 28 42 49….");
 
-        for(int i = 1; i<=10; i++)
-        {
-            int n = i * 7;
-            Console.Write(n);
-            Console.Write(" ");
-        }
+            Console.WriteLine("Введите число N");
+            int N2 = Convert.ToInt32(Console.ReadLine());
 
-        break;
+            for (int i = 1; i <= N2; i++)
+            {
+                int n = i * 7;
+                Console.Write(n);
+                Console.Write(" ");
+            }
+            Console.WriteLine("\n");
 
-    case 3:
-        Console.WriteLine("Вывод последовательности Фибоначчи");
-        break;
+            break;
 
-    default:
-        Console.WriteLine("Выбери задание от 1 до 3.");
-        break;
-}
+        case 3:
+            Console.WriteLine("Вывод последовательности Фибоначчи");
+
+            List<int> list = new();
+
+            Console.WriteLine("Введите число N");
+            int N3 = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+            for (int i = 0; i <= N3; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    Console.Write($"{i} ");
+                    list.Add(i);
+                    continue;
+                }
+                int n = list[i - 1] + list[i - 2];
+                list.Add(n);
+                Console.Write($"{n} ");
+
+            }
+            Console.WriteLine("\n");
+
+            break;
+
+        case 4:
+            return;
+
+        default:
+            Console.WriteLine("Выбери задание от 1 до 3.");
+            Console.WriteLine("\n");
+
+            break;
+    }
+} while (true);
