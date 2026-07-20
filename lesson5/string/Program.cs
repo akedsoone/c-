5cc90faf284983597de0a4a1c6992660bd7f216f
akedsoone <qupment@gmail.com>
2026-07-20 23:23:40 +0300

    наброски

--- lesson5/string/Program.cs
+++ lesson5/string/Program.cs
@@ -191,6 +191,12 @@ do
             break;
 
         case 5:
+            
+            string[] newS = Regex.Split(text, @"[!?.]+");
+            foreach(string s in newS)
+            {
+                Console.WriteLine(s);
+            }
 
             break;
 
