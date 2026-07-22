7d456585a80d3d29fd8268401c64721a7cf2440f
akedsoone <qupment@gmail.com>
2026-07-22 20:02:35 +0300

    D

---
+++ test/task1/Program.cs
@@ -0,0 +1,42 @@
+﻿class Debt
+{
+    private double balance;
+    private double interestRate;
+
+    public Debt(double initialBalance, double initialInterestRate)
+    {
+        balance = initialBalance;
+        interestRate = initialInterestRate;
+    }
+
+    public void PrintBalance()
+    {
+        Console.WriteLine($"Баланс: {balance}");
+    }
+
+    public void WaitOneYear()
+    {
+        balance = balance * interestRate;
+    }
+}
+
+class MAIN
+{
+    public static void Main(string[] args)
+    {
+        Debt mortgage = new Debt(120000.0, 1.01);
+        mortgage.PrintBalance();
+        mortgage.WaitOneYear();
+        mortgage.PrintBalance();
+
+        // Wait 20 years
+        int years = 0;
+        while (years < 20)
+        {
+            mortgage.WaitOneYear();
+            years = years + 1;
+        }
+        mortgage.PrintBalance();
+    }
+}
+
